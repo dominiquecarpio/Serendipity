0a6c9a1dba606c347045d9736560a55551f44066
dominiquecarpio <42111294+dominiquecarpio@users.noreply.github.com>
2026-04-29 08:49:48 +0000

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+// Remove loader after render
+const loader = document.getElementById('loader');
+if (loader) {
+  loader.style.display = 'none';
+}
